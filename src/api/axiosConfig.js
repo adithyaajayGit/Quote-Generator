95a06ec0e490ba071e20e2c1faeb57f4153131fe
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-07 15:45:43 +0530

    add:randomquotefn

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:'https://dummyjson.com',
+    baseURL:'https://dummyjson.com/',
     timeout:10000,
     headers:{
         'Content-Type': 'application/json',
--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import axiosInstance from '../api/axiosConfig'
 
 const Quote = () => {
+
+    const fetchrandomQuote=async()=>{
+        const response = await axiosInstance.get('quotes/random')
+    }
     return (
         <>
 
             <div className="max-w-xl mx-auto mt-10">
                 <button
                     className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-t transition duration-300 ease-in-out"
-                > click here to display quote
+                    onClick={fetchrandomQuote()}
+                > Get Another Quote
                 </button>
 
                 <div className="bg-white shadow-lg rounded-b-lg p-6 transition-all duration-300 ease-in-out">
