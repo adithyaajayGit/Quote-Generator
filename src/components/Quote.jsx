3f8b4e737070923fd61498afad96da541f25ece9
Adithya Ajay <adithyaajay098@gmail.com>
2024-09-07 15:55:34 +0530

    add:quote

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axiosInstance from '../api/axiosConfig'
 
 const Quote = () => {
@@ -17,23 +17,27 @@ const Quote = () => {
         catch(err){
             console.log("error fetching data",err)
         }
-
     }
+
+    useEffect(()=>{
+        fetchrandomQuote()
+    },[])
+
     return (
         <>
 
             <div className="max-w-xl mx-auto mt-10">
                 <button
                     className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-t transition duration-300 ease-in-out"
-                    onClick={fetchrandomQuote()}
+                    onClick={fetchrandomQuote}
                 > Get Another Quote
                 </button>
 
                 <div className="bg-white shadow-lg rounded-b-lg p-6 transition-all duration-300 ease-in-out">
-                    <h2 className="text-2xl font-bold text-gray-800 mb-2">Author name: </h2>
+                    <h2 className="text-2xl font-bold text-gray-800 mb-2">Author name: {author}</h2>
                     <div className='flex'>
                         <p className="text-2xl font-bold text-gray-800 mb-2"> Quote: </p>
-                        <p className="text-gray-600 text-sm leading-relaxed px-4 pt-1 "> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et ea vitae facilis ab doloribus debitis fugit. Itaque fugit aliquam adipisci amet ipsam beatae quisquam sit, non deserunt. Repellat, necessitatibus eligendi?</p>
+                        <p className="text-gray-600 text-sm leading-relaxed px-4 pt-1 "> {quote}</p>
                     </div>
 
                 </div>
